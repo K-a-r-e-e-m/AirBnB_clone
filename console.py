b40c01c4b351ac999e2d94698e6ff65f46f6cd6b
Kareem1715 <Kareem.H1715@gmail.com>
2024-01-13 14:03:41 +0200

    Cmd

--- console.py
+++ console.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""This module for HBNBCommand class that inherits from Cmd class 
+"""This module for HBNBCommand class that inherits from Cmd class
 from cmd module cmd.Cmd to make a command interpriter
 """
+
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """Tis  is a simple command interprete"""
+    """This  is a simple command interprete"""
 
     prompt = "(hbnb)"
 
     def emptyline(self):
+        """This method for an empty line + ENTER shouldn't execute anything"""
         pass
 
     def do_EOF(self, line):
@@ -20,6 +22,7 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         """This method handle the end of file to exit from the program"""
         return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
