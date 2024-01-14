9b90ebbd7f3be1bb947897d93f7abd32d3303892
Kareem1715 <Kareem.H1715@gmail.com>
2024-01-14 07:42:41 +0200

    Edit cmd

--- console.py
+++ console.py
@@ -4,12 +4,15 @@ from cmd module cmd.Cmd to make a command interpriter
 """
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
+from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
     """This  is a simple command interprete"""
 
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
     def emptyline(self):
         """This method for an empty line + ENTER shouldn't execute anything"""
@@ -22,6 +25,41 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         """This method handle the end of file to exit from the program"""
         return True
+
+    def do_create(self, line):
+        """This method creates a new instance of BaseModel
+        saves it to the JSON file
+        and prints the id
+        """ 
+        self.comnd = line.split()
+        if (len(self.comnd) < 2):
+            print('** class name missing **')
+        elif (self.comnd[2]):
+            print("** class doesn't exist **")
+        else:
+            storage.save()
+            #print id            
+
+    def do_show(self):
+        """This method prints the string representation of an instance
+        based on the class name and id
+        """
+
+    def do_destroy(self):
+        """This method deletes an instance based on the class name
+        and id (save the change into the JSON file)
+        """
+
+    def do_all(self):
+        """This method prints all string representation of all instances
+        based or not on the class name
+        """
+
+    def do_update(self):
+        """This method Updates an instance based on the class
+        name and id by adding or updating attribute
+        (save the change into the JSON file)
+        """
 
 
 if __name__ == '__main__':
