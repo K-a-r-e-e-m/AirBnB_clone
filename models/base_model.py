b2cc2a55bd904eb77fb6fbb0684f0071a19d08d9
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-06 15:44:31 +0300

    Task 3 - Creaet BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -14,56 +14,46 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """
-    base class
-    """
+    """Base class that defines all common attributes/methods for other classes"""
 
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """
-        Initialize a new class BaseModel.
-        """
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
+        """Initialize a new class BaseModel."""
+        self.id = str(uuid4()) # Unique ID for each base model
+        self.created_at = self.updated_at = datetime.now()
+        # if kwargs:
+        #     for key, value in kwargs.items():
+        #         if key == "created_at" or key == "updated_at":
+        #             value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+        #         if key != "__class__":
+        #             setattr(self, key, value)
 
     def save(self):
-        """
-        save method that update the time
-        """
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
-        models.storage.save()
+        """save method that update the time"""
+        self.updated_at = datetime.now()
+        # models.storage.new(self)
+        # models.storage.save()
 
     def to_dict(self):
-        """
-        Return a dictionary representation of the BaseModel instance.
-        """
-        myDict = self.__dict__.copy()
-        myDict["__class__"] = str(type(self).__name__)
-        myDict["created_at"] = self.created_at.isoformat()
-        myDict["updated_at"] = self.updated_at.isoformat()
-        myDict.pop("_sa_instance_state", None)
-        return my_dict
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        # get a copy from __dict__ to update it
+        new_dict = self.__dict__.copy()
+        # Add the class name
+        new_dict['__class__'] = self.__class__.__name__
+        # Convert the datetime objects to ISO strings
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
 
-    def delete(self):
-        """
-        Delete method
-        """
-        models.storage.delete(self)
+    # def delete(self):
+    #     """
+    #     Delete method
+    #     """
+    #     models.storage.delete(self)
 
     def __str__(self):
-        """
-        Return presentaion of the class from the user pov
-        """
-        dct = self.__dict__.copy()
-        dct.pop("_sa_instance_state", None)
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, dct)
+        """Return string representation of the class"""
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
