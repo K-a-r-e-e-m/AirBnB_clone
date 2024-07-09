0e13f4ab12470cd4b3a53840e521a4659d12ab9a
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-09 18:10:48 +0300

    Add other classes to class map dictionary

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,11 @@ to a JSON file and deserializes JSON file to instances'''
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -12,7 +17,12 @@ class FileStorage:
     __objects = {}
     class_map = {
         'BaseModel': BaseModel,
-        'User': User
+        'User': User,
+        'Place': Place,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Review': Review
     }
 
     def all(self):
