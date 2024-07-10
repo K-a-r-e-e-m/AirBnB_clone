d9d8206cf5d7c3d3e4090b1ff7586014b4ab7f1a
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-10 06:06:45 +0300

    Add new tests for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,12 @@
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import models
 
 
@@ -30,10 +36,39 @@ class TestFileStorage(unittest.TestCase):
             out_dict = file.read()
             self.assertIn(f'BaseModel.{base.id}', out_dict)
 
-    def test_file(self):    
+    def test_file_type(self):    
         '''Test type of file'''
         self.assertEqual(type(FileStorage._FileStorage__file_path), str)
     
-    def test_objects(self):
+    def test_objects_type(self):
         '''Test type of objects'''
         self.assertEqual(type(FileStorage._FileStorage__objects), dict)
+
+    def test_all_type(self):
+        self.assertEqual(dict, type(models.storage.all()))
+
+    def test_save_with_new(self):
+        '''Test save method with new instance'''
+        base = BaseModel()
+        models.storage.new(base)
+        models.storage.save()
+        with open('file.json', 'r') as file:
+            content = file.read()
+            self.assertIn(f'BaseModel.{base.id}', content)
+    
+    def test_reload_with_new_save(self):
+        '''Test reload method when create new instance and save it'''
+        base = BaseModel()
+        models.storage.new(base)
+        models.storage.save()
+        models.storage.reload()
+        myObjects =  FileStorage._FileStorage__objects
+        self.assertIn(f'BaseModel.{base.id}', myObjects)
+
+
+
+
+
+
+if __name__ == '__main_':
+    unittest.main()
